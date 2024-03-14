c14ceeb18b25abb028ab54ac9044420eff507fdb
Anon <ajeet.singh@lpu.in>
2024-03-14 06:54:11 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { config } from 'dotenv'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 
 import authRoutes from './routes/auth.routes.js'
@@ -14,7 +15,7 @@ import {app, server} from './socket/socketIo.js'
 
 config()
 const PORT = process.env.PORT || 3000
-// app.use(cors())
+const __dirname=path.resolve()
 
 app.use(cookieParser())
 app.use(express.json())
@@ -25,6 +26,10 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname,'frontend/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
+})
 server.listen(PORT,async()=>{
     await connectMongoDb()
     console.log(`Server is running on port ${PORT}`)
